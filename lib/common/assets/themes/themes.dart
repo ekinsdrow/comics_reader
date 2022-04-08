a1bdc16eef59e93cbb93944dfe99c9ef608d23ef
Ivan Kolchev <ekinsdrow@gmail.com>
2022-04-08 14:19:45 +0300

    Update light theme

--- lib/common/assets/themes/themes.dart
+++ lib/common/assets/themes/themes.dart
@@ -25,24 +25,38 @@ abstract class Themes {
       );
 
   static get lightTheme => ThemeData.light().copyWith(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.grey,
+        ),
         textTheme: const TextTheme(
           headline1: TextStyle(
             fontSize: 50,
+            color: Colors.grey,
           ),
           headline2: TextStyle(
             fontSize: 45,
+            color: Colors.grey,
           ),
           headline3: TextStyle(
             fontSize: 40,
+            color: Colors.grey,
           ),
           headline4: TextStyle(
             fontSize: 35,
           ),
           headline5: TextStyle(
             fontSize: 30,
+            color: Colors.grey,
           ),
           headline6: TextStyle(
             fontSize: 25,
+            color: Colors.grey,
+          ),
+          bodyText1: TextStyle(
+            color: Colors.grey,
+          ),
+          bodyText2: TextStyle(
+            color: Colors.grey,
           ),
         ),
       );
